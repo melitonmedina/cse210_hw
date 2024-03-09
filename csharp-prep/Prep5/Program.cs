f923e0b7928aa399596b26d6ca821bd24e2418ae
Meliton Medina <meliton.medina1@gmail.com>
2024-03-09 00:25:01 -0500

    Prep 5 complete

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,55 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int squareValue = SquareNumber(number);
+        
+        //Displaying the results
+        DisplayResults(name, squareValue);
+
+
+       
     }
+
+    // beginning of created functions
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    
+    static string PromptUserName()
+    {
+        Console.Write($"Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return(name);
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write($"Please enter you favorite number :");
+        int number = int.Parse(Console.ReadLine());
+        
+        return(number);
+    }
+
+    static int SquareNumber(int number)
+    {
+        int squared = number * number;
+
+        return(squared);
+    }
+
+    static void DisplayResults(string name, int number)
+    {
+        Console.WriteLine($"{name}, the square of your number is {number}");
+    }
+
 }
