ce02e4d307f5c2ae6ab44d1f1f12b8cf1173ce83
Meliton Medina <meliton.medina1@gmail.com>
2024-03-08 23:11:12 -0500

    Prep 4 complete

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,82 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        //needed variables
+        List<int> numList = new List<int>() ;
+        int exit = 1;
+        int newNumber;
+        double sum = 0;
+        int bigNum = 0;
+        int smallPositive = 0;
+
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (exit == 1)
+        {
+            Console.Write("Enter a number: ");
+            string answer = Console.ReadLine();
+            newNumber = int.Parse(answer);
+            if (newNumber !=0)
+            {
+                numList.Add(newNumber);
+                sum = sum + newNumber;
+
+            }
+
+          
+
+            if (newNumber == 0)
+            {
+                Console.WriteLine($"The sum is : {sum}");
+                double average = (sum/numList.Count) ;
+                //Console.WriteLine(numList.Count);
+                Console.WriteLine($"The average is : {average}");
+
+                //Finding the largest number
+                for (int i = 0; i < numList.Count; i++)
+                {
+                    if (numList[i] > 0 )
+                    {
+
+                        if (bigNum < numList[i])
+                         {
+                            bigNum = numList[i];
+                         }
+                        
+                    }
+
+                }
+
+                // finding the smalles positive number
+                for (int j = 0; j < (numList.Count - 1) ; j++)
+                {
+                    if (numList[j] > 0 )
+                    {
+                        if (numList[j] > numList[j +1])
+                        {
+                            smallPositive = numList[j+1];
+
+                        }
+                        else
+                        {
+                            smallPositive = numList[j];
+                        }
+                           
+                    }
+                    
+                }
+
+
+                Console.WriteLine($"The largest number is: {bigNum}");
+                Console.WriteLine($"The smallest positive number is : {smallPositive} ");              
+                exit = 2;
+            }
+            
+
+
+        }
+
+        }
+
     }
-}
