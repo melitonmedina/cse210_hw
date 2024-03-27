6257f97a8d9fe9b6da703c6e38b899edf92f8a8d
Mel <meliton.medina1@gmail.com>
2024-03-27 00:49:18 -0400

    prep 5 complete

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Net.Sockets;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square sqr1 = new Square("red", 12.5);
+        Circle crl1 = new Circle("blue", 7.0);
+        Rectangle rect1 = new Rectangle("brown", 10.7, 5.5);
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(sqr1);
+        shapes.Add(crl1);
+        shapes.Add(rect1);
+
+        foreach (Shape s in shapes)
+        {
+            Console.Write($"{s.GetColor()} ");
+            Console.Write($"{s.GetArea()}\n");
+        }
+       
+      
     }
 }
